f537031822febe1e922ff95e53749c3748ab07ff
Thanh-Hang Tran <hangtran2015@gmail.com>
2025-12-05 18:34:26 -0600

    Update scripts.js - Time-Based Event

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@
 
 function displayDate() {
   const today = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
-  document.getElementById("dateDisplay").innerText = "Today Date: " + today.toLocaleDateString("en-US", options);
+  const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  document.getElementById("dateDisplay").innerText = "Today Date: " + now.toLocaleDateString("en-US", options);
   applyPatternValidation();
   setDOBRange();
   validateDOBOnBlur();
